ccaf4ba90e6a5e552741fdf5dcad9826ec227bbb
Panu Kalliokoski <panu.kalliokoski@sange.fi>
2025-03-17 14:30:59 +0200

    handling the response

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def root():
     return render_template('lomake.html')
+
+@app.route("/vastaus")
+def vastaus():
+    return render_template('vastaus.html', nimi=request.args['nimi'])
 
